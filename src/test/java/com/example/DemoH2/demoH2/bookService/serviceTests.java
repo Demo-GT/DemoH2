6d2302eca4e3f36b22fdc31442ad47140d253e75
Demo-GT <itzmiiigt@gmail.com>
2021-01-05 19:50:32 +0530

    new cases are added

--- src/test/java/com/example/DemoH2/demoH2/bookService/serviceTests.java
+++ src/test/java/com/example/DemoH2/demoH2/bookService/serviceTests.java
@@ -8,8 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -19,12 +18,31 @@ public class serviceTests {
     bookService Bs;
 
     @Test
-    public void SampleRUN() {
-
-        book B = new book(1, "uegsf", "jaudgw");
+    public void TestInsert() {
+        book B = new book("uegsf", "jaudgw");
         Bs.insertBook(B);
-        book result = Bs.getBookById(1);
+        book result = Bs.getBookById(2);
         assertEquals(B.getId(), result.getId());
-
     }
+
+    @Test
+    public void TestUpdate() {
+        book B = new book("qwert","564");
+        Bs.updateBook(2,B);
+        book result = Bs.getBookById(2);
+        assertEquals(B.getGenre(),result.getGenre());
+    }
+    
+    @Test
+    public void TestDelete() {
+        Bs.insertBook(new book("uyq","qu"));
+        assertTrue(Bs.deleteBook(1)==1);
+    }
+
+    @Test void TestGetAll() {
+        assertTrue((Bs.getAllBooks()).size() >= 0);
+    }
+
 }
+
+
